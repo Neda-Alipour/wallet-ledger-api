22491faee264d7a6737e28ae85711f7949d6d898
Neda Alipour <neda.alipour.dev@gmail.com>
2026-08-10 16:18:49 +0400

    Refactor authentication schemas - Remove unused Signup and Login schemas and their dependencies

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, EmailStr, Field, field_validator
-from fastapi import Form
 import re
 
 class AuthBase(BaseModel):
@@ -16,33 +15,3 @@ class AuthBase(BaseModel):
         if not re.search(r"[0-9]", password):
             raise ValueError("Password must contain at least one number")
         return password
-
-    @classmethod
-    def as_form(
-        cls,
-        email: EmailStr = Form(...),
-        password: str = Form(...)
-    ):
-        return cls(email=email, password=password)
-
-class SignupSchema(AuthBase):
-    """Inherits everything from AuthBase"""
-    pass
-
-class LoginSchema(AuthBase):
-    """Inherits everything from AuthBase"""
-    pass
-
-# class SignupSchema(AuthBase):
-#     # 1. Add the new field specific to Signup
-#     user_name: str = Field(..., min_length=3, max_length=20)
-
-#     # 2. Override as_form to include user_name
-#     @classmethod
-#     def as_form(
-#         cls,
-#         email: EmailStr = Form(...),
-#         password: str = Form(...),
-#         user_name: str = Form(...) # Add this parameter
-#     ):
-#         return cls(email=email, password=password, user_name=user_name)
--- app/schemas/dependencies.py
+++ app/schemas/dependencies.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import Session
 
 from app.db.session import get_db
 from app.models.user import User
-from app.schemas.auth import LoginSchema, SignupSchema
 
 from app.services.auth import AuthService
 from app.core.security import oauth2_scheme
@@ -44,7 +43,5 @@ UserDep = Annotated[
 # why Annotated not just Session = Depends(get_db)? because we want to specify the type of db parameter as Session for better type hinting and editor support 
 
 # Define a reusable type
-SignupDep = Annotated[SignupSchema, Depends(SignupSchema.as_form)] 
-LoginDep = Annotated[LoginSchema, Depends(LoginSchema.as_form)] 
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
